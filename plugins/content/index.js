0230366a90d9fce4b646d5b180ea7572c0c345f7
Ludovic Muller <ludovicmuller1@gmail.com>
2022-12-20 22:52:14 +0100

    feat(content): remove big comment

--- plugins/content/index.js
+++ plugins/content/index.js
@@ -7,61 +7,6 @@ import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
 
 /**
-```yaml
-config:
-  namespace: default
-  entries:
-    # home page
-    - name: about-lindas
-      paths:
-        default: file:content/about-lindas/default.md # if no default, return empty value
-        fr: file:content/about-lindas/fr.md
-        en: file:content/about-lindas/en.md
-        de: file:content/about-lindas/de.md
-        it: file:content/about-lindas/it.md
-    - name: linked-data
-      paths:
-        default: file:content/linked-data/default.md # if no default, return empty value
-        fr: file:content/linked-data/fr.md
-        en: file:content/linked-data/en.md
-        de: file:content/linked-data/de.md
-        it: file:content/linked-data/it.md
-
-    - name: datacatalog
-      paths:
-        default: file:content/datacatalog/default.md # if no default, return empty value
-        fr: file:content/datacatalog/fr.md
-        en: file:content/datacatalog/en.md
-        de: file:content/datacatalog/de.md
-        it: file:content/datacatalog/it.md
-
-    - name: query-data
-      paths:
-        default: file:content/query-data/default.md # if no default, return empty value
-        fr: file:content/query-data/fr.md
-        en: file:content/query-data/en.md
-        de: file:content/query-data/de.md
-        it: file:content/query-data/it.md
-
-    - name: publish-data
-      paths:
-        default: file:content/publish-data/default.md # if no default, return empty value
-        fr: file:content/publish-data/fr.md
-        en: file:content/publish-data/en.md
-        de: file:content/publish-data/de.md
-        it: file:content/publish-data/it.md
-
-    - name: contact
-      paths:
-        default: file:content/contact/default.md # if no default, return empty value
-        fr: file:content/contact/fr.md
-        en: file:content/contact/en.md
-        de: file:content/contact/de.md
-        it: file:content/contact/it.md
-```
-**/
-
-/**
  * Return a HTML string from a Markdown string.
  *
  * @param {string} markdownString
