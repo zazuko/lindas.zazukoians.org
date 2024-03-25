859f09ffee797e53ff62381376bca4bee62ab767
Ludovic Muller <ludovicmuller1@gmail.com>
2024-03-25 16:02:35 +0100

    chore: upgrade to Trifid v5

--- plugins/menu/index.js
+++ plugins/menu/index.js
@@ -1,24 +1,22 @@
 const factory = async (trifid) => {
-  const { config, logger } = trifid
+  const { config, logger, server } = trifid
   const { namespace, entries } = config
 
   if (!entries || !Array.isArray(entries)) {
-    throw new Error(`'entries' should be a non-empty array`)
+    throw new Error('\'entries\' should be a non-empty array')
   }
 
-  const configuredNamespace = namespace ?? 'default';
+  const configuredNamespace = namespace ?? 'default'
   const store = []
-
-  let i = 0
 
   for (const entry of entries) {
     const { path, label } = entry
     if (!path || typeof path !== 'string') {
-      throw new Error(`'path' should be a non-empty string`)
+      throw new Error('\'path\' should be a non-empty string')
     }
 
     if (!label || typeof label !== 'string') {
-      throw new Error(`'label' should be a non-empty string`)
+      throw new Error('\'label\' should be a non-empty string')
     }
 
     store.push({
@@ -27,20 +25,19 @@ const factory = async (trifid) => {
     })
   }
 
-  return (_req, res, next) => {
-    logger.debug(`loaded menu into '${configuredNamespace}' namespace`)
-
-    // just make sure that the `menu` entry exists
-    if (!res.locals.menu) {
-      res.locals.menu = {}
-    }
-
-    // add all configured entries for the specified namespace
-    res.locals.menu[configuredNamespace] = store
-
-    // let's forward all of this to other middlewares
-    return next()
+  const locals = server.locals
+  if (!locals) {
+    throw new Error('locals not found')
   }
+
+  if (!locals.has('menu')) {
+    locals.set('menu', {})
+  }
+
+  const currentMenu = locals.get('menu')
+  currentMenu[configuredNamespace] = store
+  locals.set('menu', currentMenu)
+  logger.debug(`loaded menu into '${configuredNamespace}' namespace`)
 }
 
 export default factory
